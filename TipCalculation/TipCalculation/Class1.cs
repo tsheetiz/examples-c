231f5c24bbb38e9fdd30c48de94e60a0b1d759b3
tsheetiz <35674379+tsheetiz@users.noreply.github.com>
2019-01-04 17:07:20 -0500

    Add files via upload

---
+++ TipCalculation/TipCalculation/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculation
+{
+    public class Tipper
+    {
+        public double total_bill;
+
+        public double CalculateTip(double tip_percentage)
+        {
+            return tip_percentage * total_bill;
+        }
+
+        /*
+         * Note that this is method overloading since the two CalculateTip methods have the same name. 
+         * This is legal in C# since the parameter lists are different (i.e. one of the methods takes 1 parameter and the other takes 0 parameters). 
+        */
+
+        public double CalculateTip()
+        {
+            return total_bill * (.15);
+        }
+    }
+}
