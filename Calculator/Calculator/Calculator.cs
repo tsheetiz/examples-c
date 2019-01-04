94249bbfbb5d4aa220a6ab2cfbae934ea42a51b9
tsheetiz <tsheetiz@vt.edu>
2019-01-04 18:23:12 -0500

    starting example

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public float Answer;
+
+        public void Add(float number1, float number2)
+        {
+            Answer = number1 + number2;
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("First number? ");
+            float first = float.Parse(Console.ReadLine());
+            Console.Write("Second number? ");
+            float second = float.Parse(Console.ReadLine());
+
+            // TODO: create a Calculator object
+
+            while (true)
+            {
+                Console.Write("A)dd S)ubtract M)ultiply D)ivide Q)uit: ");
+                var operation = Console.ReadLine().ToUpper()[0];
+
+                if (operation == 'Q')
+                    break;
+
+                switch (operation)
+                {
+                    case 'A':
+                        // TODO: add the two numbers with the calculator
+                        break;
+                    case 'S':
+                        // TODO: subtract the two numbers with the calculator
+                        break;
+                    case 'M':
+                        // TODO: multiply the two numbers with the calculator
+                        break;
+                    case 'D':
+                        // TODO: divide the two numbers with the calculator
+                        break;
+                    default:
+                        Console.WriteLine("Please enter A, S, M, D or Q");
+                        break;
+                }
+                // TODO: output the result from the calculator
+            }
+        }
+    }
+}
