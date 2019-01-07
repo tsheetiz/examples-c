aacf8e2fe9e4e93f21c1a7ee5b502ba92ed755d7
tsheetiz <tsheetiz@vt.edu>
2019-01-06 23:41:28 -0500

    yee

---
+++ ConstructorsApp/ConstructorsApp/Constructors.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsApp
+{
+    public class BankAccount
+    {
+        private double balance;
+        private DateTime lastDeposit;
+        private bool isActive;
+        private string accountName;
+
+        // TODO: add constructors
+
+        public BankAccount(string accountName, DateTime lastDeposit, double balance = 0)
+        {
+            this.balance = balance;
+            this.lastDeposit = lastDeposit;
+            this.accountName = accountName;
+
+            isActive = true;
+        }
+
+        public BankAccount(string accountName) : this(accountName, DateTime.Now)
+        { 
+
+        }
+
+        public BankAccount() : this("Unkown Account", new DateTime())
+        {
+            isActive = false;
+        }
+
+        public string GetAccountInfo()
+        {
+            return String.Format("{0}:  Balance: {1}. Last Deposit: {2}, IsActive: {3}",
+                accountName,
+                balance,
+                lastDeposit,
+                isActive);
+        }
+    }
+}
---
+++ ConstructorsApp/ConstructorsApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: use different constructors for accounts.
+            BankAccount account1 = new BankAccount();
+
+            BankAccount account2 = new BankAccount("College Fund");
+
+            BankAccount account3 = new BankAccount("Checking", new DateTime(2014, 7, 10, 15, 0, 0), 50000);
+
+
+            Console.WriteLine(account1.GetAccountInfo());
+
+            Console.WriteLine(account2.GetAccountInfo());
+
+            Console.WriteLine(account3.GetAccountInfo());
+        }
+    }
+}
