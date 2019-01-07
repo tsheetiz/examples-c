7dc7390134fb1029ae5336955cbaff316ea79a88
tsheetiz <tsheetiz@vt.edu>
2019-01-06 23:40:11 -0500

    finished

---
+++ Bank/Bank/Bank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Bank
+    {
+        // TODO: Create GetAccountInfo method with the following specs:
+        // Parameters:
+        //   - accountNumber (string)
+        //   - balance (double) ***
+        //   - lastDeposit (DateTime) ***
+        // Return type:
+        //   - bool
+
+        public bool GetAccountInfo(string accountNumber, out double balance, out DateTime lastDeposit)
+        {
+            balance = 24127.84;
+            lastDeposit = new DateTime(2014, 11, 22, 14, 35, 20);
+
+            bool isActive = true;
+            return isActive;
+        }
+
+        //Note: Parameters with *** should be defined as out parameters
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+
+            // TODO: Get account information
+
+            double balance;
+            DateTime lastDeposit;
+
+            bool activeAccount = bank.GetAccountInfo("1021379", out balance, out lastDeposit);
+
+            // TODO: Print account information (replace ? with variables)
+
+            PrintAccountInfo(activeAccount, balance, lastDeposit);
+
+        }
+        static void PrintAccountInfo(bool isActive, double balance, DateTime lastDeposit)
+        {
+            Console.WriteLine("Account is active? {0}", isActive);
+            Console.WriteLine("Balance: {0}", balance.ToString("C"));
+            Console.WriteLine("Date of last deposit: {0}", lastDeposit);
+        }
+    }
+}
